e3530e1f72dc0596cbbe0d09f0b33bee9d6d5d0a
Halil Can CALISKAN <61836866+halilcancaliskan@users.noreply.github.com>
2023-06-13 00:10:42 +0200

    first commit

---
+++ server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import { Request, Response } from 'express';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, world!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
