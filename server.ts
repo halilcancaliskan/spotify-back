c168e3f2c91d936e66dc06dc696fb115b41a0eed
Halil Can CALISKAN <61836866+halilcancaliskan@users.noreply.github.com>
2023-06-13 22:35:34 +0200

    express ok

--- server.ts
+++ server.ts
@@ -1,24 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import { Request, Response } from 'express';
+import express, { Request, Response } from 'express';
+
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://spotifyback:spotifyback@clusterspotify.nw1zahb.mongodb.net/';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-// Middleware
-app.use(cors());
-app.use(helmet());
-app.use(morgan('dev'));
-app.use(express.json());
-
-// Routes
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, world!');
-});
-
-// Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Serveur Express écoutant sur le port ${port}`);
 });
